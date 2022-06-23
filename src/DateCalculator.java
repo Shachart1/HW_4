d90aac9a078dab4fcd36c7532633f3232a72b087
Shachart1 <shachart@campus.technion.ac.il>
2022-06-23 13:36:28 +0300

    split

--- src/DateCalculator.java
+++ src/DateCalculator.java
@@ -5,9 +5,9 @@ public class DateCalculator {
             return date;
         }
         if(num > 0){
-            return addToDate(progress(date,1),num-1);
+            return addToDate(progressDay(date,1),num-1);
         }
-        return addToDate(progress(date,-1),num+1);
+        return addToDate(progressDay(date,-1),num+1);
     }
 
     /**
@@ -16,7 +16,7 @@ public class DateCalculator {
      * @param sign - 1 if we want to add a day and -1 if we want to decrease a day
      * @return - updated date
      */
-    private Date progressDay(Date date,int sign){
+    private static Date progressDay(Date date,int sign){
         int year = date.getYear();
         int month = date.getMonth() - 1;
         int day = date.getDay()+sign;
@@ -37,13 +37,13 @@ public class DateCalculator {
      * @param year - current year
      * @return the progressed date
      */
-    private Date progressYear(int day, int month, int year){
+    private static Date progressYear(int day, int month, int year){
         if(month > Months.DECEMBER.ordinal()){ // need to add a Year
             day = 1; // 1st in January
             month = Months.JANUARY.ordinal();
             year ++;
         }
-        else { // need to decrease year
+        if() { // need to decrease year
             day = 31; // 31st in December
             month = Months.DECEMBER.ordinal();
             year --;
@@ -51,19 +51,24 @@ public class DateCalculator {
         return new Date(day, month + 1, year); // adding 1 to month since we decreased it before to use enum
     }
 
+
+    private static numDays(int month){
+
+    }
+
     /**
      * checking if a year is a leap year
      * @param year - the current year
      * @return true - year is leap year, false - leap is regular year
      */
-    private boolean isLeapYear(int year){
+    private static boolean isLeapYear(int year){
         if((year % 400 == 0) || (year % 4 == 0 && year % 100 != 0)){
             return true;
         }
         return false;
     }
 
-    private Date progressMonth(int day,Months month, int year){
+    private static Date progressMonth(int day,int month, int year){
   switch (month){
       case (month)
   }
